4ab35a7edd30756a8e3ada8a3a6dbf1a5467813a
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-06-19 13:19:48 +0530

    day 2

---
+++ Concatnation.js
@@ -0,0 +1,8 @@
+/*concatination*/
+let name = "Arjun";
+let age = 23;
+
+let message = "my name is "+name+ " and i am " + age;
+console.log(message);
+
+
---
+++ arithmetic.js
@@ -0,0 +1,9 @@
+const x = 12;
+const y = 13;
+console.log(x + y);
+console.log(x * y);
+console.log(x / y);
+console.log(x - y);
+const x1 = "re";
+const x2 = "build";
+console.log(x1 * x2);
